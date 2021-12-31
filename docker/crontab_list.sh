1951ccaea097ef93d904431670d4e1893a7b471a
HolmesLiu <273667951@qq.com>
2021-12-31 08:50:02 +0800

    猪圈更新

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -131,7 +131,7 @@
 # 天天提鹅
 18 * * * * node /scripts/jd_daily_egg.js >> /scripts/logs/jd_daily_egg.log 2>&1
 # 金融养猪
-12 * * * * node /scripts/jd_pigPet.js >> /scripts/logs/jd_pigPet.log 2>&1
+12 3-24/2 * * * node /scripts/jd_pigPet.js >> /scripts/logs/jd_pigPet.log 2>&1
 # 京喜工厂
 20 * * * * node /scripts/jd_dreamFactory.js >> /scripts/logs/jd_dreamFactory.log 2>&1
 # 京喜工厂开团
