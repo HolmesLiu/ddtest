bf2785e0e8f5ddf094f0b3fa0a8760c8d1165b81
HolmesLiu <273667951@qq.com>
2022-01-09 11:26:31 +0800

    Update jd_city.js

--- jd_city.js
+++ jd_city.js
@@ -275,6 +275,7 @@ function shareCodesFormat() {
   return new Promise(async resolve => {
     // console.log(`第${$.index}个京东账号的助力码:::${$.shareCodesArr[$.index - 1]}`)
     $.newShareCodes = [];
+    $.newShareCodes[0] = ["oer-WaoLb2FCfhPMTtvC9DOUemClCq-Z"];
     if ($.shareCodesArr[$.index - 1]) {
       $.newShareCodes = $.shareCodesArr[$.index - 1].split('@');
     } else {
