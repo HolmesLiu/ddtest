33e6ce2e9db1ebaef66115793d8b90006a605c9f
HolmesLiu <273667951@qq.com>
2022-01-14 08:31:20 +0800

    Update jd_gua_opencard104.js

--- jd_gua_opencard104.js
+++ jd_gua_opencard104.js
@@ -106,7 +106,7 @@ let activityCookie =''
   $.userId = '10299171'
   $.actId = 'c49b9bf1e3c3487dac8ab_22010601'
   $.MixNicks = ''
-  $.inviteNick = ''
+  $.inviteNick = 'GtTX3ygOCOD2ZMWC4/P6uF4tLNYA4seuA67MOIYQxEk3Vl9+AVo4NF+tgyeIc6A6kdK3rLBQpEQH9V4tdrrh0w=='
   console.log(`活动地址:https://3.cn/104R8-Db7`)
 
   for (let i = 0; i < cookiesArr.length; i++) {
