a3028be49edb222536d109420fa9c6c69cc95edb
HolmesLiu <273667951@qq.com>
2022-01-21 17:10:26 +0800

    Update jd_opencardL69.js

--- jd_opencardL69.js
+++ jd_opencardL69.js
@@ -69,7 +69,7 @@ let activityCookie =''
     });
     return;
   }
-  $.shareUuid = ""
+  $.shareUuid = "b834785ab6da449eafe2aaf966980b90"
 
   for (let i = 0; i < cookiesArr.length; i++) {
     cookie = cookiesArr[i];
@@ -216,7 +216,7 @@ async function run() {
     console.log($.actorUuid)
     console.log(`当前助力:${$.shareUuid}`)
     if($.index == 1){
-      $.shareUuid = $.actorUuid
+      $.shareUuid = $.shareUuid
       console.log(`后面的号都会助力:${$.shareUuid}`)
     }
     await $.wait(parseInt(Math.random() * 1000 + 5000, 10))
